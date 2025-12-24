fa09ae7131e545fd33480ac84b7011c473662181
Emre Boyacı <emre.boyaci@kariyer.net>
2025-12-24 16:03:14 +0300

    👾Character limit removed

--- Sources/InfomaniakRichHTMLEditor/Models/UserScript.swift
+++ Sources/InfomaniakRichHTMLEditor/Models/UserScript.swift
@@ -32,7 +32,6 @@ extension UserScript {
         UserScript(name: "captureLog", injectionTime: .atDocumentStart),
         UserScript(name: "javascriptBridge", injectionTime: .atDocumentStart),
         UserScript(name: "utils", injectionTime: .atDocumentStart),
-        UserScript(name: "characterLimit", injectionTime: .atDocumentEnd),
 
         // Editor
         UserScript(name: "text-attributes", injectionTime: .atDocumentStart),
